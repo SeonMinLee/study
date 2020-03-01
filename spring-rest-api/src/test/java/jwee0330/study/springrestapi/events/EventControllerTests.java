7dd1c6511de5f7f3be92eccb4d0bafafb1b1e428
jwee0330 <jwee0330@gmail.com>
2020-03-02 02:22:34 +0900

    feat: 입력값 제한하기-1

--- spring-rest-api/src/test/java/jwee0330/study/springrestapi/events/EventControllerTests.java
+++ spring-rest-api/src/test/java/jwee0330/study/springrestapi/events/EventControllerTests.java
@@ -1,6 +1,8 @@
 package jwee0330.study.springrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -34,6 +36,7 @@ public class EventControllerTests {
     @Test
     public void createEvent() throws Exception {
         Event event = Event.builder()
+                .id(100)
                 .name("Spring")
                 .description("REST API Development with Spring")
                 .beginEnrollmentDateTime(LocalDateTime.of(2020,03,02,02,00,00))
@@ -44,8 +47,8 @@ public class EventControllerTests {
                 .maxPrice(200)
                 .limitOfEnrollment(100)
                 .location("강남역 D2 스타텁 팩토리")
+                .free(true)
                 .build();
-        event.setId(10);
         when(eventRepository.save(event))
                 .thenReturn(event);
 
@@ -57,7 +60,8 @@ public class EventControllerTests {
                 .andExpect(status().isCreated())
                 .andExpect(header().exists(HttpHeaders.LOCATION))
                 .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-                .andExpect(jsonPath("id").exists())
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
         ;
     }
 }
