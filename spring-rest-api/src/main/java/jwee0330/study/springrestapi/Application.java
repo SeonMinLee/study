fb896ef5d8748500ebfa23da513a0a7a537b7262
jwee0330 <jwee0330@gmail.com>
2020-03-02 01:33:15 +0900

    init spring-rest-api

---
+++ spring-rest-api/src/main/java/jwee0330/study/springrestapi/Application.java
@@ -0,0 +1,13 @@
+package jwee0330.study.springrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
