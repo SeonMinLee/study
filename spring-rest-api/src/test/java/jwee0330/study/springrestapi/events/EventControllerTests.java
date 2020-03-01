a798b1af4de56ca2a4821680daffc798a2475971
jwee0330 <jwee0330@gmail.com>
2020-03-02 02:40:02 +0900

    test: 입력값 이외에 에러 발생

--- spring-rest-api/src/test/java/jwee0330/study/springrestapi/events/EventControllerTests.java
+++ spring-rest-api/src/test/java/jwee0330/study/springrestapi/events/EventControllerTests.java
@@ -2,6 +2,7 @@ package jwee0330.study.springrestapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -27,10 +28,10 @@ public class EventControllerTests {
     @Autowired
     ObjectMapper objectMapper;
 
+    @DisplayName("POST /api/events 201")
     @Test
     public void createEvent() throws Exception {
-        Event event = Event.builder()
-                .id(100)
+        EventDto event = EventDto.builder()
                 .name("Spring")
                 .description("REST API Development with Spring")
                 .beginEnrollmentDateTime(LocalDateTime.of(2020, 03, 02, 02, 00, 00))
@@ -41,8 +42,6 @@ public class EventControllerTests {
                 .maxPrice(200)
                 .limitOfEnrollment(100)
                 .location("강남역 D2 스타텁 팩토리")
-                .eventStatus(EventStatus.PUBLISHED)
-                .free(true)
                 .build();
 //        when(eventRepository.save(event))
 //                .thenReturn(event);
@@ -58,6 +57,34 @@ public class EventControllerTests {
                 .andExpect(jsonPath("id").value(Matchers.not(100)))
                 .andExpect(jsonPath("free").value(Matchers.not(true)))
                 .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.PUBLISHED)))
+        ;
+    }
+
+    @DisplayName("POST /api/events 400")
+    @Test
+    public void createEvent_badRequest() throws Exception {
+        Event event = Event.builder()
+                .id(100)
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020, 03, 02, 02, 00, 00))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020, 03, 02, 04, 00, 00))
+                .beginEventDateTime(LocalDateTime.of(2020, 03, 05, 00, 00, 00))
+                .endEventDateTime(LocalDateTime.of(2020, 03, 06, 00, 00, 00))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .eventStatus(EventStatus.PUBLISHED)
+                .free(true)
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                .content(objectMapper.writeValueAsBytes(event))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
         ;
     }
 }
