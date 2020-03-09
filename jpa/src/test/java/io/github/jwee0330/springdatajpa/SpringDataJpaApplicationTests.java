aa5f4f86779337d2108da774145e3f97eacfaa45
jwee0330 <jwee0330@gmail.com>
2020-03-09 22:04:45 +0900

    polishing

---
+++ jpa/src/test/java/io/github/jwee0330/springdatajpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jwee0330.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
