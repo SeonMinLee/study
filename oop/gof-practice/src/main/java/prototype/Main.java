77e76945ee345f3a54eeda33a6f4e9f9af51b470
jwee0330 <jwee0330@gmail.com>
2020-03-29 16:40:18 +0900

    feat(ProtoTypePattern): 프로토타입 패턴 내용 정리하고 샘플코드 작성한다

---
+++ oop/gof-practice/src/main/java/prototype/Main.java
@@ -0,0 +1,29 @@
+package prototype;
+
+import prototype.anonymous.MessageBox;
+import prototype.anonymous.UnderlinePen;
+import prototype.framework.Manager;
+import prototype.framework.Product;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+        UnderlinePen underlinePen = new UnderlinePen('~');
+        MessageBox mBox = new MessageBox('*');
+        MessageBox sBox = new MessageBox('/');
+
+        manager.register("string message", underlinePen);
+        manager.register("warning box", mBox);
+        manager.register("slash box", sBox);
+
+        Product p1 = manager.create("string message");
+        p1.use("hello world!");
+
+        Product p2 = manager.create("warning box");
+        p2.use("hello world!");
+
+        Product p3 = manager.create("slash box");
+        p3.use("hello world!");
+    }
+}
